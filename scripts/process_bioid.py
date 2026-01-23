9e6cc1f7e0a1f4a4be8b7d8c7ef58e7f6c5a72d6
buz galbraith <buzgalbraith@gmail.com>
2026-01-23 14:28:44 -0500

    added list gilda score

--- scripts/process_bioid.py
+++ scripts/process_bioid.py
@@ -1,7 +1,6 @@
 import polars as pl
 from typing import Tuple
 from functools import lru_cache
-from lxml import etree
 from rcps_og.utils.constants import CALIBRATION_DATA_PATH
 from rcps_og.utils.utils import (
     get_gilda_predictions,
@@ -15,7 +14,7 @@ import pystow
 import subprocess
 import os
 import numpy as np
-
+from pathlib import Path
 MODULE = pystow.module("gilda", "biocreative")
 URL = "https://github.com/buzgalbraith/BioCreative-VI-Track-1/raw/refs/heads/main/data/BioIDtraining_2.tar.gz"  # used to be ('https://biocreative.bioinformatics.udel.edu/media/store/files/2017/BioIDtraining_2.tar.gz')
 BIOID_PATH = "~/.data/BioIDtraining_2/gilda_dataset.tsv"
@@ -23,7 +22,7 @@ BIOID_PATH = "~/.data/BioIDtraining_2/gilda_dataset.tsv"
 import polars as pl 
 import pystow
 from functools import lru_cache
-from lxml import etree
+import xml.etree.ElementTree as etree
 import gilda
 from rapidfuzz import fuzz
 from typing import Callable
@@ -34,14 +33,13 @@ URL = "https://github.com/buzgalbraith/BioCreative-VI-Track-1/raw/refs/heads/mai
 BIOID_PATH = "~/.data/BioIDtraining_2/gilda_dataset.tsv"
 
 
-def safe_max_score(scores:list[float]|list[None])->float:
-    if len(scores) > 0:
+def safe_max_score(scores:list[float])->float:
+    if scores: ## evaluates to false if empty list  
         return max(scores)
-    else:
-        ## if there is no list
+    else: 
         return 0.0
-def safe_min_loss(scores:list[float]|list[None])->float:
-    if len(scores) > 0:
+def safe_min_loss(scores:list[float])->float:
+    if scores: ## evaluates to false if empty list  
         return min(scores)
     else:
         ## if there is no list
@@ -57,6 +55,11 @@ def fuzzy_string_score_list(entity: str, candidate_names: list[str], agg_method:
         score = fuzz.ratio(entity, candidate_name) / 100
         scores.append(score)
     return agg_method(scores)
+def gilda_score_list(entity: str, gilda_scores: list[float], agg_method:Callable[[list[float]], float]=safe_max_score) -> float:
+    """
+    aggregate the existing Gilda scores. Aggregate scores with a function
+    """
+    return agg_method(gilda_scores)
 
 def binary_miscoverage_loss_list(labels: list[str], candidate_set: list[str], agg_method:Callable[[list[float]], float]=safe_min_loss) -> float:
     """
@@ -84,7 +87,7 @@ def get_plaintext(don_article: str) -> str:
     path = directory.joinpath('BioIDtraining_2', 'fulltext_bioc',
                                 f'{don_article}.xml')
     tree = etree.parse(path.as_posix())
-    paragraphs = tree.xpath('//text')
+    paragraphs = tree.findall('//text')
     paragraphs = [' '.join(text.itertext()) for text in paragraphs]
     return '/n'.join(paragraphs) + '/n'
 
@@ -120,8 +123,8 @@ def get_gilda_candidates_bioid(text:str, don_article:str)->list[mini_gilda_match
 
 
 def split_calibration_and_validation(
-    base_df_path: str = CALIBRATION_DATA_PATH,
-    base_df: pl.DataFrame = None,
+    base_df_path: Path = CALIBRATION_DATA_PATH,
+    base_df: pl.DataFrame|None = None,
     split_col="document_id",
     validate_prop: float = 0.2,
 ) -> Tuple[pl.DataFrame, pl.DataFrame]:
@@ -178,21 +181,27 @@ if __name__ == "__main__":
         )
     )
     bio_id_df = bio_id_df.with_columns(
-        loss = pl.struct(['obj_synonyms', 'gilda_curie']).map_elements(
-            lambda x: binary_miscoverage_loss_list(x['obj_synonyms'], x['gilda_curie'], agg_method=safe_min_loss), 
+        score = pl.struct(['text', 'gilda_scores']).map_elements(
+            lambda x: gilda_score_list(x['text'], x['gilda_scores'], agg_method=safe_max_score), 
             return_dtype=pl.Float64
         )
     )
-    ## there are still cases where gilda is getting mappings that we are not using a custom grounder ##
-    bio_id_df = bio_id_df.with_columns(
-        numeric_exists = pl.col('exists_correct').cast(dtype=pl.Float64),
-    )
-    bio_id_df.filter(pl.col('numeric_exists').eq(pl.col('loss'))).select(
-        [
-            'obj_synonyms',
-            'groundings',
-            'gilda_curie', 
-            'numeric_exists',
-            'loss'
-        ]
-    )
+    # bio_id_df = bio_id_df.with_columns(
+    #     loss = pl.struct(['obj_synonyms', 'gilda_curie']).map_elements(
+    #         lambda x: binary_miscoverage_loss_list(x['obj_synonyms'], x['gilda_curie'], agg_method=safe_min_loss), 
+    #         return_dtype=pl.Float64
+    #     )
+    # )
+    # ## there are still cases where gilda is getting mappings that we are not using a custom grounder ##
+    # bio_id_df = bio_id_df.with_columns(
+    #     numeric_exists = pl.col('exists_correct').cast(dtype=pl.Float64),
+    # )
+    # bio_id_df.filter(pl.col('numeric_exists').eq(pl.col('loss'))).select(
+    #     [
+    #         'obj_synonyms',
+    #         'groundings',
+    #         'gilda_curie', 
+    #         'numeric_exists',
+    #         'loss'
+    #     ]
+    # )
